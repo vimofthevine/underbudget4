a8671fa89a7d65c71a0154e41c23c6ae998dcdd8
Kyle Treubig <kyle@vimofthevine.com>
2019-12-22 23:39:39 +0000

    Add backend spring boot application from Spring Initializr

---
+++ backend/src/test/kotlin/com.vimofthevine.underbudget/UnderbudgetApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vimofthevine.underbudget
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UnderbudgetApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
