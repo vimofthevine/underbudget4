1642018c12ee91c5f2fb8c65f3b234124cf7da6b
Kyle Treubig <kyle@vimofthevine.com>
2020-01-01 20:48:21 +0000

    Test the user repository

--- backend/src/main/kotlin/com.vimofthevine.underbudget/model/User.kt
+++ backend/src/main/kotlin/com.vimofthevine.underbudget/model/User.kt
@@ -6,6 +6,7 @@ import java.util.UUID
 import javax.persistence.*
 
 @Entity
+@Table(name = "user")
 data class User(
   @Id
   @GeneratedValue
---
+++ backend/src/test/kotlin/com.vimofthevine.underbudget/repository/UserRepositoryTest.kt
@@ -0,0 +1,71 @@
+package com.vimofthevine.underbudget.repository
+
+import com.vimofthevine.underbudget.model.User
+
+import java.util.UUID
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.slf4j.LoggerFactory
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.beans.factory.annotation.Autowired
+
+@DataJpaTest
+class UserRepositoryTest {
+  private val logger = LoggerFactory.getLogger(javaClass)
+
+  @Autowired
+  lateinit var repo: UserRepository
+
+  @Test
+  fun shouldFindByIdWhenUserExists() {
+    val id = repo.save(User(
+      name = "Test User",
+      email = "user@test.com",
+      hashedPassword = "password"
+    )).id
+    val user = repo.findById(id)
+    assertTrue(user.isPresent)
+    assertEquals("Test User", user.get().name)
+    assertEquals("user@test.com", user.get().email)
+  }
+
+  @Test
+  fun shouldNotFindByIdWhenUserDoesNotExist() {
+    assertFalse(repo.findById(UUID.randomUUID()).isPresent)
+  }
+
+  @Test
+  fun shouldFindByNameWhenUserExists() {
+    repo.save(User(
+      name = "Test User",
+      email = "user@test.com",
+      hashedPassword = "password"
+    ))
+    val user = repo.findByName("Test User")
+    assertEquals("Test User", user?.name)
+    assertEquals("user@test.com", user?.email)
+  }
+
+  @Test
+  fun shouldNotFindByNameWhenUserDoesNotExist() {
+    assertNull(repo.findByName("Test User"))
+  }
+
+  @Test
+  fun shouldFindByEmailWhenUserExists() {
+    repo.save(User(
+      name = "Test User",
+      email = "user@test.com",
+      hashedPassword = "password"
+    ))
+    val user = repo.findByEmail("user@test.com")
+    assertEquals("Test User", user?.name)
+    assertEquals("user@test.com", user?.email)
+  }
+
+  @Test
+  fun shouldNotFindByEmailWhenUserDoesNotExist() {
+    assertNull(repo.findByEmail("user@test.com"))
+  }
+}
