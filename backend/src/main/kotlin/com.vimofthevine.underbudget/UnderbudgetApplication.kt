e5173956400a7f8d0108b56d0b33e167b4332589
Kyle Treubig <kyle@vimofthevine.com>
2020-01-11 17:09:45 +0000

    Remove JPA annotation from main class to allow unit testing of controllers

--- backend/src/main/kotlin/com.vimofthevine.underbudget/UnderbudgetApplication.kt
+++ backend/src/main/kotlin/com.vimofthevine.underbudget/UnderbudgetApplication.kt
@@ -2,10 +2,8 @@ package com.vimofthevine.underbudget
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 @SpringBootApplication
-@EnableJpaRepositories
 class UnderbudgetApplication
 
 fun main(args: Array<String>) {
