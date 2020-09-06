7ca2b98fc4121683b64169d546d2ffe0a1125eac
Kyle Treubig <kyle@vimofthevine.com>
2020-09-06 14:48:01 +0000

    Upgrade storybook to 6.0

--- webapp/.storybook/preview.js
+++ webapp/.storybook/preview.js
@@ -5,7 +5,7 @@ import { addDecorator } from '@storybook/react';
 import React from 'react';
 import { useDarkMode } from 'storybook-dark-mode';
 
-import createTheme from '../src/utils/createTheme';
+import createTheme from '../src/common/utils/createTheme';
 
 addDecorator(story => (
   <ThemeProvider theme={createTheme(useDarkMode())}>
