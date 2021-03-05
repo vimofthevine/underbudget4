3ea6dc695dc95d4df1560e9efb2496d40eee9c4b
Kyle Treubig <kyle@vimofthevine.com>
2021-03-05 07:43:45 -0500

    Add create-demo dialog to ledgers page

--- webapp/src/ledgers/components/LedgersPage/LedgersPage.js
+++ webapp/src/ledgers/components/LedgersPage/LedgersPage.js
@@ -1,7 +1,9 @@
 import AddCircleIcon from '@material-ui/icons/AddCircle';
+import AddIcon from '@material-ui/icons/Add';
 import React from 'react';
 
 import FullAppPage from '../../../common/components/FullAppPage';
+import CreateDemoLedgerDialog from '../CreateDemoLedgerDialog';
 import CreateLedgerDialog from '../CreateLedgerDialog';
 import { useLedgersDispatch } from '../LedgersContext';
 import LedgersListing from '../LedgersListing';
@@ -12,14 +14,23 @@ const LedgersPage = () => {
 
   const createLedger = {
     'aria-label': 'Create ledger',
+    fabIcon: <AddIcon />,
     icon: <AddCircleIcon />,
     onClick: () => dispatch({ type: 'showCreateLedger' }),
     text: 'Create ledger',
   };
 
+  const createDemo = {
+    'aria-label': 'Create demo',
+    icon: <AddCircleIcon />,
+    onClick: () => dispatch({ type: 'showCreateDemoLedger' }),
+    text: 'Create demo',
+  };
+
   return (
-    <FullAppPage primaryActions={createLedger} title='Ledgers' useFab>
+    <FullAppPage primaryActions={createLedger} secondaryActions={createDemo} title='Ledgers' useFab>
       <LedgersListing />
+      <CreateDemoLedgerDialog />
       <CreateLedgerDialog />
       <ModifyLedgerDialog />
     </FullAppPage>
