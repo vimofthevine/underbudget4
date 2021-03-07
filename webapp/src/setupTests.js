5690728a6b0fab2ecd12a9592cade082cd849437
Kyle Treubig <kyle@vimofthevine.com>
2021-03-07 12:25:01 -0500

    Use custom getElementError so we don't print out the full document on query errors

--- webapp/src/setupTests.js
+++ webapp/src/setupTests.js
@@ -4,10 +4,26 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
 
+import { configure, getConfig } from '@testing-library/react';
 import { setLogger } from 'react-query';
 
 setLogger({
   log: () => 0,
   warn: () => 0,
   error: () => 0,
+});
+
+const origGetElementError = getConfig().getElementError;
+configure({
+  getElementError: (message, container) => {
+    // Even though this should only ever be the document.body or a specific container,
+    // when we use the Material-UI Autocomplete component this gets reset to the top-level
+    // document, which results in a huge prettyDOM printout that includes all the injected
+    // MUI JSS in the document head that is not useful for test debugging. So we set it
+    // back to document.body if that occurs.
+    if (container === document) {
+      container = document.body;
+    }
+    return origGetElementError(message, container);
+  },
 });
