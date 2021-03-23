2876a675d8a7f59137b696a75745b1699bb5f0c0
Kyle Treubig <kyle@vimofthevine.com>
2021-03-23 07:53:20 -0400

    Treat location changes as storybook actions

--- webapp/.storybook/preview.js
+++ webapp/.storybook/preview.js
@@ -1,12 +1,13 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { withA11y } from '@storybook/addon-a11y';
+import { action } from '@storybook/addon-actions';
 import { addDecorator } from '@storybook/react';
 import React from 'react';
-import { MemoryRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { useDarkMode } from 'storybook-dark-mode';
 
+import useMemoryRouter from '../src/common/hooks/useMemoryRouter';
 import createTheme from '../src/common/utils/createTheme';
 
 addDecorator((story, { parameters }) => {
@@ -19,14 +20,14 @@ addDecorator((story, { parameters }) => {
   });
 
   const { initialRoute = '/' } = parameters;
+  const { history, routerProps, Router } = useMemoryRouter({ initialEntries: [initialRoute] });
+  React.useEffect(() => history.listen((location) => action('navigate')(location)), [history]);
 
   return (
     <ThemeProvider theme={createTheme(useDarkMode())}>
       <CssBaseline />
       <QueryClientProvider client={queryClient}>
-        <MemoryRouter initialEntries={[initialRoute]}>
-          {story()}
-        </MemoryRouter>
+        <Router {...routerProps}>{story()}</Router>
       </QueryClientProvider>
     </ThemeProvider>
   );
