b084d3f4bfc3a17b925d8932c1913a56bb478260
Kyle Treubig <kyle@vimofthevine.com>
2021-04-01 07:50:34 -0400

    Add account deletion action to accounts list

--- webapp/src/accounts/hooks/useDeleteAccount.js
+++ webapp/src/accounts/hooks/useDeleteAccount.js
@@ -1,30 +1,14 @@
-import { useMutation } from 'react-query';
+import axios from 'axios';
 
-import useConfirmation from '../../common/hooks/useConfirmation';
 import useErrorMessage from '../../common/hooks/useErrorMessage';
-import useSnackbar from '../../common/hooks/useSnackbar';
-import deleteAccount from '../api/deleteAccount';
-import useAccountsRefetch from './useAccountsRefetch';
+import useMutation from '../../common/hooks/useMutation';
+import useSelectedLedger from '../../ledgers/hooks/useSelectedLedger';
 
-export default function useDeleteAccount(account) {
-  const confirm = useConfirmation();
-  const snackbar = useSnackbar();
-  const refetch = useAccountsRefetch();
-
-  const createErrorMessage = useErrorMessage({ request: 'Unable to delete account' });
-
-  const { mutate } = useMutation(deleteAccount, {
-    onError: (err) => snackbar(createErrorMessage(err)),
-    onSuccess: refetch,
+export default (opts) => {
+  const ledger = useSelectedLedger();
+  return useMutation((id) => axios.delete(`/api/accounts/${id}`), {
+    createErrorMessage: useErrorMessage({ request: 'Unable to delete account' }),
+    refetchQueries: [['account-categories', { ledger }]],
+    ...opts,
   });
-
-  return () =>
-    confirm({
-      message: [
-        `Delete account ${account.name}?`,
-        'This action is permanent and cannot be undone.',
-      ],
-    }).then(() => {
-      mutate(account.id);
-    });
-}
+};
