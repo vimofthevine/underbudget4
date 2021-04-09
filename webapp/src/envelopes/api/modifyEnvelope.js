629e5218d2ab4990668fcb483029d716997c2062
Kyle Treubig <kyle@vimofthevine.com>
2021-04-09 18:55:08 -0400

    Remove old envelope API utils

--- webapp/src/envelopes/api/modifyEnvelope.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-import getAuthHeaders from '../../common/utils/getAuthHeaders';
-
-export default ({ id, ...data }) =>
-  axios.patch(`/api/envelopes/${id}`, data, {
-    headers: getAuthHeaders(),
-  });
