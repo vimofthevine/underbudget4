117bd7618c762c5244d638fde85eb0b049d5d218
Kyle Treubig <kyle@vimofthevine.com>
2021-04-09 19:24:15 -0400

    Update create envelope dialog

--- webapp/src/envelopes/components/CreateEnvelopeDialog/CreateEnvelopeDialog.test.js
+++ webapp/src/envelopes/components/CreateEnvelopeDialog/CreateEnvelopeDialog.test.js
@@ -6,30 +6,10 @@ import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import renderWithRouter from '../../../tests/renderWithRouter';
-import { EnvelopeContextProvider } from '../../contexts/envelope';
-import useCreateEnvelope from '../../hooks/useCreateEnvelope';
 import CreateEnvelopeDialog from './CreateEnvelopeDialog';
 
-const OpenDialogButton = () => (
-  <button onClick={useCreateEnvelope()} type='button'>
-    Open
-  </button>
-);
-
 const render = () => {
-  localStorage.setItem('underbudget.selected.ledger', 'ledger-id');
-
-  const mock = new MockAdapter(axios);
-  mock
-    .onGet('/api/ledgers/ledger-id/envelopeCategories?projection=categoryWithEnvelopes')
-    .reply(200, {
-      _embedded: {
-        envelopeCategories: [
-          { id: 'cat-id-1', name: 'Category 1' },
-          { id: 'cat-id-2', name: 'Category 2' },
-        ],
-      },
-    });
+  localStorage.setItem('underbudget.selected.ledger', '2');
 
   const queryClient = new QueryClient({
     defaultOptions: {
@@ -39,15 +19,20 @@ const render = () => {
     },
   });
 
+  const mock = new MockAdapter(axios);
+  mock
+    .onGet('/api/ledgers/2/envelope-categories')
+    .reply(200, {
+      categories: [
+        { id: 1, name: 'Category 1' },
+        { id: 2, name: 'Category 2' },
+      ],
+    });
+
   return {
     ...renderWithRouter(
       <QueryClientProvider client={queryClient}>
-        <EnvelopeContextProvider>
-          <>
-            <OpenDialogButton />
-            <CreateEnvelopeDialog />
-          </>
-        </EnvelopeContextProvider>
+        <CreateEnvelopeDialog />
       </QueryClientProvider>,
     ),
     mock,
@@ -55,93 +40,57 @@ const render = () => {
   };
 };
 
-describe('CreateEnvelopeDialog', () => {
-  it('should prevent submission when required fields are missing', async () => {
-    const { mock } = render();
+test('should prevent submission when required fields are missing', async () => {
+  const { mock } = render();
 
-    userEvent.click(screen.getByRole('button', { name: 'Open' }));
-    await waitFor(() =>
-      expect(screen.getByRole('heading', { name: /create envelope/i })).toBeInTheDocument(),
-    );
-    await waitFor(() => expect(mock.history.get.length).toBe(1));
+  expect(screen.getByRole('heading', { name: /create envelope/i })).toBeInTheDocument();
+  await waitFor(() => expect(mock.history.get.length).toBe(1));
 
-    const createButton = screen.getByRole('button', { name: /create/i });
-    userEvent.click(createButton);
-    await waitFor(() => expect(screen.getAllByText(/required/i)).toHaveLength(2));
+  const createButton = screen.getByRole('button', { name: /create/i });
+  userEvent.click(createButton);
 
-    expect(createButton).toBeDisabled();
+  await waitFor(() => expect(screen.getAllByText(/required/i)).toHaveLength(2));
+  expect(createButton).toBeDisabled();
+});
+
+test('should show error message when request error', async () => {
+  const { mock } = render();
+  mock.onPost('/api/envelope-categories/2/envelopes').reply(400);
+
+  expect(screen.getByRole('heading', { name: /create envelope/i })).toBeInTheDocument();
+  await waitFor(() => expect(mock.history.get.length).toBe(1));
+
+  userEvent.type(screen.getByLabelText(/^name/i), 'my envelope name');
+  userEvent.click(screen.getByRole('button', { name: /open/i }));
+  userEvent.click(screen.getByRole('option', { name: 'Category 2' }));
+  userEvent.click(screen.getByRole('button', { name: /create/i }));
+
+  await waitFor(() => expect(screen.getByText(/unable to create envelope/i)).toBeInTheDocument());
+});
+
+test('should close and refresh query when successful create', async () => {
+  const { mock, queryClient } = render();
+  mock.onPost('/api/envelope-categories/2/envelopes').reply(201);
+  const invalidateQueries = jest.spyOn(queryClient, 'invalidateQueries');
+
+  expect(screen.getByRole('heading', { name: /create envelope/i })).toBeInTheDocument();
+  await waitFor(() => expect(mock.history.get.length).toBe(1));
+
+  userEvent.type(screen.getByLabelText(/^name/i), 'my envelope name');
+  userEvent.click(screen.getByRole('button', { name: /open/i }));
+  userEvent.click(screen.getByRole('option', { name: 'Category 2' }));
+  userEvent.click(screen.getByRole('button', { name: /create/i }));
+
+  await waitFor(() =>
+    expect(screen.queryByRole('heading', { name: /create envelope/i })).not.toBeInTheDocument(),
+  );
+  expect(JSON.parse(mock.history.post[0].data)).toEqual({
+    name: 'my envelope name',
   });
-
-  it('should show error message when request error', async () => {
-    const { mock } = render();
-    mock.onPost('/api/envelopes').reply(400);
-
-    userEvent.click(screen.getByRole('button', { name: 'Open' }));
-    await waitFor(() =>
-      expect(screen.getByRole('heading', { name: /create envelope/i })).toBeInTheDocument(),
-    );
-    await waitFor(() => expect(mock.history.get.length).toBe(1));
-
-    await userEvent.type(screen.getByLabelText(/^name/i), 'my envelope name');
-    userEvent.click(screen.getByRole('button', { name: /open/i }));
-    userEvent.click(screen.getByRole('option', { name: 'Category 2' }));
-    userEvent.click(screen.getByRole('button', { name: /create/i }));
-
-    await waitFor(() => expect(screen.getByText(/unable to create envelope/i)).toBeInTheDocument());
-  });
-
-  it('should close and refresh query when successful create', async () => {
-    const { mock, queryClient } = render();
-    mock.onPost('/api/envelopes').reply(201);
-    const invalidateQueries = jest.spyOn(queryClient, 'invalidateQueries');
-
-    userEvent.click(screen.getByRole('button', { name: 'Open' }));
-    await waitFor(() =>
-      expect(screen.getByRole('heading', { name: /create envelope/i })).toBeInTheDocument(),
-    );
-    await waitFor(() => expect(mock.history.get.length).toBe(1));
-
-    await userEvent.type(screen.getByLabelText(/^name/i), 'my envelope name');
-    userEvent.click(screen.getByRole('button', { name: /open/i }));
-    userEvent.click(screen.getByRole('option', { name: 'Category 2' }));
-    userEvent.click(screen.getByRole('button', { name: /create/i }));
-
-    await waitFor(() =>
-      expect(screen.queryByRole('heading', { name: /create envelope/i })).not.toBeInTheDocument(),
-    );
-    expect(JSON.parse(mock.history.post[0].data)).toEqual({
-      category: '/api/envelope-categories/cat-id-2',
-      name: 'my envelope name',
-    });
-    expect(invalidateQueries).toHaveBeenCalledWith([
-      'envelopeCategories',
-      {
-        ledger: 'ledger-id',
-      },
-    ]);
-  });
-
-  it('should send all populated fields', async () => {
-    const { mock } = render();
-    mock.onPost('/api/envelopes').reply(201);
-
-    userEvent.click(screen.getByRole('button', { name: 'Open' }));
-    await waitFor(() =>
-      expect(screen.getByRole('heading', { name: /create envelope/i })).toBeInTheDocument(),
-    );
-    await waitFor(() => expect(mock.history.get.length).toBe(1));
-
-    await userEvent.type(screen.getByLabelText(/^name/i), 'my envelope name');
-    userEvent.click(screen.getByRole('button', { name: /open/i }));
-    userEvent.click(screen.getByRole('option', { name: 'Category 2' }));
-    userEvent.click(screen.getByRole('button', { name: /create/i }));
-
-    await waitFor(() =>
-      expect(screen.queryByRole('heading', { name: /create envelope/i })).not.toBeInTheDocument(),
-    );
-    expect(JSON.parse(mock.history.post[0].data)).toEqual({
-      category: '/api/envelope-categories/cat-id-2',
-      name: 'my envelope name',
-    });
-  });
+  expect(invalidateQueries).toHaveBeenCalledWith([
+    'envelope-categories',
+    {
+      ledger: '2',
+    },
+  ]);
 });
