a6c29e6ee6049a3a4a3482caab26d72a0ca9d46e
Kyle Treubig <kyle@vimofthevine.com>
2021-04-25 17:25:20 -0400

    Add account title and balance to page header

--- webapp/src/accounts/components/AccountTransactionsPage/AccountTransactionsPage.js
+++ webapp/src/accounts/components/AccountTransactionsPage/AccountTransactionsPage.js
@@ -3,13 +3,21 @@ import ArchiveIcon from '@material-ui/icons/Archive';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import React from 'react';
+import { useParams } from 'react-router-dom';
 // import { Routes, Route } from 'react-router-dom';
 
 import FullAppPage from '../../../common/components/FullAppPage';
 import useNavigateKeepingSearch from '../../../common/hooks/useNavigateKeepingSearch';
+import useFormatMoney from '../../../ledgers/hooks/useFormatMoney';
+import useFetchAccount from '../../hooks/useFetchAccount';
+import useFetchAccountBalance from '../../hooks/useFetchAccountBalance';
 
 const AccountTransactionsPage = () => {
+  const formatMoney = useFormatMoney();
   const navigate = useNavigateKeepingSearch();
+  const { id } = useParams();
+  const { data } = useFetchAccount({ id });
+  const { data: balanceData } = useFetchAccountBalance({ id });
 
   const primaryActions = [
     {
@@ -41,12 +49,18 @@ const AccountTransactionsPage = () => {
     },
   ];
 
+  const title = React.useMemo(() => {
+    if (!data) {
+      return 'Account';
+    }
+    if (!balanceData) {
+      return data.name;
+    }
+    return `${data.name} | ${formatMoney(balanceData.balance)}`;
+  }, [data, balanceData]);
+
   return (
-    <FullAppPage
-      primaryActions={primaryActions}
-      secondaryActions={secondaryActions}
-      title='Account'
-    >
+    <FullAppPage primaryActions={primaryActions} secondaryActions={secondaryActions} title={title}>
       <div>account transactions</div>
     </FullAppPage>
   );
