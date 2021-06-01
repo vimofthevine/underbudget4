2b5b9b2e8c46620d79aed673f7cbfece0cd0fa65
Kyle Treubig <kyle@vimofthevine.com>
2021-06-01 07:56:29 -0400

    Create money input field component

---
+++ webapp/src/common/components/MoneyInputField/MoneyInputField.js
@@ -0,0 +1,31 @@
+import getSymbolFromCurrency from 'currency-symbol-map';
+// import PropTypes from 'prop-types';
+import React from 'react';
+
+import useSelectedLedgerCurrency from '../../hooks/useSelectedLedgerCurrency';
+import NumberInputField from '../NumberInputField';
+
+const MoneyInputField = (props) => {
+  const {
+    currency: { code, digits },
+    isValid,
+  } = useSelectedLedgerCurrency();
+  const symbol = React.useMemo(() => getSymbolFromCurrency(code), [code]);
+  const fromValue = (v) => v / 10 ** digits;
+  const toValue = (v) => v * 10 ** digits;
+  return (
+    <NumberInputField
+      fromValue={fromValue}
+      numberInputProps={{
+        decimalScale: digits,
+        fixedDecimalScale: true,
+        prefix: isValid ? symbol : null,
+        thousandSeparator: true,
+      }}
+      toValue={toValue}
+      {...props}
+    />
+  );
+};
+
+export default MoneyInputField;
