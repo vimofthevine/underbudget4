6ca814e3b73c903aee7f75089b61cecbdc647b96
Kyle Treubig <kyle@vimofthevine.com>
2021-06-08 07:45:32 -0400

    Use account/envelope ID as array field in query keys for transactions

--- webapp/src/transactions/hooks/useFetchAccountTransactions.js
+++ webapp/src/transactions/hooks/useFetchAccountTransactions.js
@@ -9,7 +9,7 @@ export default ({ defaultSize = 25, id }) => {
 
   return {
     ...useQuery(
-      ['account-transactions', { id, page, size }],
+      ['account-transactions', id, { page, size }],
       async () => {
         const { data } = await axios.get(
           `/api/accounts/${id}/transactions?page=${page}&size=${size}`,
