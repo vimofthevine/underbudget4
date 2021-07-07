26d264c6f08b81148a3f223e12aaa1991e58d25c
Kyle Treubig <kyle@vimofthevine.com>
2021-07-07 07:57:14 -0400

    Disable setting drawer state in localstorage for mobile

--- webapp/src/common/hooks/useMobile.js
+++ webapp/src/common/hooks/useMobile.js
@@ -3,5 +3,5 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 
 export default () => {
   const theme = useTheme();
-  return useMediaQuery(theme.breakpoints.down('xs'));
+  return useMediaQuery(theme.breakpoints.down('xs'), { noSsr: true });
 };
