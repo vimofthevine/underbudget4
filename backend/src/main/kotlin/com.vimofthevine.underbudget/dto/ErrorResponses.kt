30dd525f483489f0bf9d07c42fbaeefee51b6a15
Kyle Treubig <kyle@vimofthevine.com>
2021-07-13 07:57:22 -0400

    Remove backend kotlin source and Maven pom files

--- backend/src/main/kotlin/com.vimofthevine.underbudget/dto/ErrorResponses.kt
+++
@@ -1,6 +0,0 @@
-package com.vimofthevine.underbudget.dto
-
-data class ErrorResponse(
-  val message: String,
-  val details: List<String>? = null
-)
--- backend/src/main/kotlin/com.vimofthevine.underbudget/repository/EnvelopeRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.vimofthevine.underbudget.repository
-
-import com.vimofthevine.underbudget.model.Envelope
-
-import java.util.UUID
-
-import org.springframework.data.repository.PagingAndSortingRepository
-
-interface EnvelopeRepository : PagingAndSortingRepository<Envelope, UUID>
