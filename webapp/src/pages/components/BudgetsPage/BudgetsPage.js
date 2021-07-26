0a7e16b9a5694c828840f60d28c6b2822f7b664e
Kyle Treubig <kyle@vimofthevine.com>
2021-07-26 07:49:17 -0400

    Set up tabs for budgets page

--- webapp/src/pages/components/BudgetsPage/BudgetsPage.js
+++ webapp/src/pages/components/BudgetsPage/BudgetsPage.js
@@ -1,9 +1,37 @@
+import Paper from '@material-ui/core/Paper';
+import Tab from '@material-ui/core/Tab';
+import TabContext from '@material-ui/lab/TabContext';
+import TabList from '@material-ui/lab/TabList';
+import TabPanel from '@material-ui/lab/TabPanel';
 import React from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 import FullAppPage from '../../../common/components/FullAppPage';
 
 const BudgetsPage = () => {
-  return <FullAppPage title='Budgets'>all budgets</FullAppPage>;
+  const [searchParams, setSearchParams] = useSearchParams({ tab: 'active' });
+  const tabValue = searchParams.get('tab');
+  const handleChangeTab = (e, tab) => setSearchParams({ tab });
+
+  return (
+    <FullAppPage title='Budgets'>
+      <TabContext value={tabValue}>
+        <Paper>
+          <TabList
+            aria-label='budget tabs'
+            centered
+            indicatorColor='primary'
+            onChange={handleChangeTab}
+          >
+            <Tab value='active' label='Active' />
+            <Tab value='all' label='All' />
+          </TabList>
+        </Paper>
+        <TabPanel value='active'>Active Budgets</TabPanel>
+        <TabPanel value='all'>All Budgets</TabPanel>
+      </TabContext>
+    </FullAppPage>
+  );
 };
 
 export default BudgetsPage;
