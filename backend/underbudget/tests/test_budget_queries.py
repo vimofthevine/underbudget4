3cd9e51e56fc0806b192a96d0c0012946e10db40
Kyle Treubig <kyle@vimofthevine.com>
2021-08-02 08:39:00 -0400

    Add API to query for budgeted expenses on any budget

--- backend/underbudget/tests/test_budget_queries.py
+++ backend/underbudget/tests/test_budget_queries.py
@@ -18,7 +18,7 @@ class BudgetQueriesTestCase(BaseTestCase):
             ("auto", 2021, 2),
         ]
     )
-    def test_budgeted_expenses_when_budget_not_found(self, ledger, year, period):
+    def test_active_budgeted_expenses_when_budget_not_found(self, ledger, year, period):
         ledger_id = self.create_ledger()
         budget_id = self.create_budget(ledger_id, periods=2)
         self.create_active_budget(ledger_id, budget_id, year=2021)
@@ -33,7 +33,7 @@ class BudgetQueriesTestCase(BaseTestCase):
             == 404
         )
 
-    def test_budget_expenses_include_all_expenses_for_period(self):
+    def test_active_budget_expenses_include_all_expenses_for_period(self):
         ledger_id = self.create_ledger()
         budget_id = self.create_budget(ledger_id, periods=4)
         self.create_active_budget(ledger_id, budget_id, year=2021)
@@ -78,6 +78,82 @@ class BudgetQueriesTestCase(BaseTestCase):
         }
 
         resp = self.client.get(f"/api/ledgers/{ledger_id}/budgeted-expenses/2021/3")
+        assert resp.status_code == 200
+        assert resp.json == {
+            "expensesByEnvelopeId": {
+                str(env_1_id): 88,
+                str(env_2_id): 110,
+                str(env_3_id): 120,
+            }
+        }
+
+    @parameterized.expand(
+        [
+            ("not-an-id", 1),
+            (999, 1),
+            ("auto", "not-a-period"),
+            ("auto", -1),
+            ("auto", 2),
+        ]
+    )
+    def test_budgeted_expenses_when_budget_not_found(self, budget, period):
+        ledger_id = self.create_ledger()
+        budget_id = self.create_budget(ledger_id, periods=2)
+
+        if budget == "auto":
+            budget = budget_id
+
+        assert (
+            self.client.get(
+                f"/api/budgets/{budget}/budgeted-expenses/{period}"
+            ).status_code
+            == 404
+        )
+
+    def test_budget_expenses_include_all_expenses_for_period(self):
+        ledger_id = self.create_ledger()
+        budget_id = self.create_budget(ledger_id, periods=4)
+        env_cat_id = self.create_envelope_category(ledger_id)
+        env_1_id = self.create_envelope(env_cat_id)
+        env_2_id = self.create_envelope(env_cat_id)
+        env_3_id = self.create_envelope(env_cat_id)
+        self.create_periodic_expense(budget_id, env_1_id, amount=75)
+        self.create_periodic_expense(budget_id, env_2_id, amount=50)
+        self.create_periodic_expense(budget_id, env_3_id, amount=120)
+        self.create_annual_expense(budget_id, env_2_id, amount=240)
+        self.create_annual_expense(budget_id, env_1_id, details=[17, 21, 0, 13])
+
+        resp = self.client.get(f"/api/budgets/{budget_id}/budgeted-expenses/0")
+        assert resp.status_code == 200
+        assert resp.json == {
+            "expensesByEnvelopeId": {
+                str(env_1_id): 92,
+                str(env_2_id): 110,
+                str(env_3_id): 120,
+            }
+        }
+
+        resp = self.client.get(f"/api/budgets/{budget_id}/budgeted-expenses/1")
+        assert resp.status_code == 200
+        assert resp.json == {
+            "expensesByEnvelopeId": {
+                str(env_1_id): 96,
+                str(env_2_id): 110,
+                str(env_3_id): 120,
+            }
+        }
+
+        resp = self.client.get(f"/api/budgets/{budget_id}/budgeted-expenses/2")
+        assert resp.status_code == 200
+        assert resp.json == {
+            "expensesByEnvelopeId": {
+                str(env_1_id): 75,
+                str(env_2_id): 110,
+                str(env_3_id): 120,
+            }
+        }
+
+        resp = self.client.get(f"/api/budgets/{budget_id}/budgeted-expenses/3")
         assert resp.status_code == 200
         assert resp.json == {
             "expensesByEnvelopeId": {
--- backend/underbudget/views/budget_queries.py
+++ backend/underbudget/views/budget_queries.py
@@ -14,31 +14,40 @@ def register(app: Flask):
     """ Registers all views """
     app.add_url_rule(
         "/api/ledgers/<int:ledger_id>/budgeted-expenses/<int:year>/<int:period>",
+        view_func=active_budgeted_expenses_by_period,
+        methods=["GET"],
+    )
+    app.add_url_rule(
+        "/api/budgets/<int:budget_id>/budgeted-expenses/<int:period>",
         view_func=budgeted_expenses_by_period,
         methods=["GET"],
     )
 
 
-def budgeted_expenses_by_period(ledger_id: int, year: int, period: int):
+def active_budgeted_expenses_by_period(ledger_id: int, year: int, period: int):
     """ Retrieve all budgeted expenses for the specified period """
     active_budget = ActiveBudgetModel.find_by_year(ledger_id, year)
     if not active_budget:
         raise NotFound("No active budget found")
+    return budgeted_expenses_by_period(active_budget.budget_id, period)
 
-    budget = BudgetModel.query.get_or_404(active_budget.budget_id)
+
+def budgeted_expenses_by_period(budget_id: int, period: int):
+    """ Retrieve all budgeted expenses for the specified period """
+    budget = BudgetModel.query.get_or_404(budget_id)
     if period < 0 or period >= budget.periods:
         raise NotFound("Invalid period")
 
     expenses_by_envelope = {}
 
-    periodic_expenses = BudgetPeriodicExpenseModel.find_by_budget_id(active_budget.id)
+    periodic_expenses = BudgetPeriodicExpenseModel.find_by_budget_id(budget.id)
     for expense in periodic_expenses:
         if expense.envelope_id in expenses_by_envelope:
             expenses_by_envelope[expense.envelope_id] += expense.amount
         else:
             expenses_by_envelope[expense.envelope_id] = expense.amount
 
-    annual_expenses = BudgetAnnualExpenseModel.find_by_budget_id(active_budget.id)
+    annual_expenses = BudgetAnnualExpenseModel.find_by_budget_id(budget.id)
     for expense in annual_expenses:
         if len(expense.details) > period:
             amount = expense.details[period].amount
