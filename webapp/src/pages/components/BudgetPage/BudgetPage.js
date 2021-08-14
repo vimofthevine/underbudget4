5540070e487d093095868d6439c8eff9cd0cc61b
Kyle Treubig <kyle@vimofthevine.com>
2021-08-14 09:13:51 -0400

    Implement deletion of budget

--- webapp/src/pages/components/BudgetPage/BudgetPage.js
+++ webapp/src/pages/components/BudgetPage/BudgetPage.js
@@ -1,26 +1,52 @@
+import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import React from 'react';
 import { Routes, Route, useLocation, useParams } from 'react-router-dom';
 
-import FullAppPage from 'common/components/FullAppPage';
-import useNavigateKeepingSearch from 'common/hooks/useNavigateKeepingSearch';
-import * as routes from 'common/utils/routes';
 import ExpenseSummary from 'budgets/components/ExpenseSummary';
 import IncomeSummary from 'budgets/components/IncomeSummary';
 import ModifyBudgetDialog from 'budgets/components/ModifyBudgetDialog';
+import useFetchActiveBudgets from 'budgets/hooks/useFetchActiveBudgets';
 import useFetchBudget from 'budgets/hooks/useFetchBudget';
+import FullAppPage from 'common/components/FullAppPage';
+import useConfirmation from 'common/hooks/useConfirmation';
+import useNavigateKeepingSearch from 'common/hooks/useNavigateKeepingSearch';
+import * as routes from 'common/utils/routes';
+import useDeleteBudget from 'budgets/hooks/useDeleteBudget';
 
 const BudgetPage = () => {
+  const confirm = useConfirmation();
   const { state } = useLocation();
   const { budgetsPageSearch = '' } = state || {};
   const navigate = useNavigateKeepingSearch();
 
   const { id } = useParams();
   const { data } = useFetchBudget({ id });
+  const { budgets: activeBudgets } = useFetchActiveBudgets();
 
   const parentRoute = React.useMemo(
     () => ({ pathname: routes.BUDGETS, search: budgetsPageSearch }),
     [budgetsPageSearch],
+  );
+
+  const isActive = React.useMemo(() => {
+    const idAsInt = parseInt(id, 10);
+    return !!activeBudgets.find((b) => b.budgetId === idAsInt);
+  }, [activeBudgets, id]);
+
+  const { mutate: deleteBudget } = useDeleteBudget({
+    onSuccess: () => navigate(parentRoute),
+  });
+
+  const handleDelete = React.useCallback(
+    () =>
+      confirm({
+        message: [
+          `Delete budget ${data && data.name}?`,
+          'This action is permanent and cannot be undone.',
+        ],
+      }).then(() => deleteBudget(id)),
+    [data, id],
   );
 
   const primaryActions = [
@@ -29,6 +55,13 @@ const BudgetPage = () => {
       icon: <EditIcon />,
       onClick: () => navigate('modify'),
       text: 'Modify',
+    },
+    {
+      'aria-label': 'Delete budget',
+      disabled: isActive,
+      icon: <DeleteIcon />,
+      onClick: handleDelete,
+      text: 'Delete',
     },
   ];
 
--- webapp/src/pages/components/BudgetPage/BudgetPage.test.js
+++ webapp/src/pages/components/BudgetPage/BudgetPage.test.js
@@ -9,7 +9,7 @@ import renderWithRouter from 'tests/renderWithRouter';
 import setupMockApi from 'tests/setupMockApi';
 import BudgetPage from './BudgetPage';
 
-const render = (route = '/budget/7') => {
+const render = ({ isActive = false, route = '/budget/7' } = {}) => {
   configure({ defaultHidden: true });
 
   setSelectedLedger('2');
@@ -18,6 +18,14 @@ const render = (route = '/budget/7') => {
   mockApi.onGet('/api/budgets/7').reply(200, {
     name: 'Test Budget',
     periods: 12,
+  });
+  mockApi.onGet('/api/ledgers/2/active-budgets').reply(200, {
+    activeBudgets: [
+      {
+        id: 5,
+        budgetId: isActive ? 7 : 17,
+      },
+    ],
   });
 
   const queryClient = new QueryClient({
@@ -43,7 +51,7 @@ const render = (route = '/budget/7') => {
 };
 
 test('should display modify-budget dialog if initial route matches', async () => {
-  const { history } = render('/budget/7/modify');
+  const { history } = render({ route: '/budget/7/modify' });
   expect(screen.getByRole('heading', { name: /modify budget/i })).toBeInTheDocument();
 
   await waitFor(() => expect(screen.getByRole('button', { name: /cancel/i })).toBeInTheDocument());
@@ -67,4 +75,47 @@ test('should open dialogs when using nav bar actions', async () => {
     expect(screen.getByRole('heading', { name: /modify budget/i })).toBeInTheDocument(),
   );
   expect(history.location.pathname).toBe('/budget/7/modify');
+});
+
+test('should disable delete action if budget is used as active', async () => {
+  render({ isActive: true });
+  await waitFor(() => expect(screen.getByRole('button', { name: /delete/i })).toBeDisabled());
+});
+
+test('should prompt to confirm deletion of budget', async () => {
+  const { history, mockApi, queryClient } = render();
+  const invalidateQueries = jest.spyOn(queryClient, 'invalidateQueries');
+  mockApi.onDelete('/api/budgets/7').reply(204);
+
+  await waitFor(() =>
+    expect(screen.getByRole('heading', { name: 'Test Budget' })).toBeInTheDocument(),
+  );
+
+  // Reject cancellation
+  userEvent.click(screen.getByRole('button', { name: /delete/i }));
+  await waitFor(() =>
+    expect(screen.getByRole('heading', { name: /confirm/i })).toBeInTheDocument(),
+  );
+  userEvent.click(screen.getByRole('button', { name: /cancel/i }));
+
+  await waitFor(() =>
+    expect(screen.queryByRole('heading', { name: /confirm/i })).not.toBeInTheDocument(),
+  );
+  expect(mockApi.history.delete).toHaveLength(0);
+
+  // Confirm cancellation
+  userEvent.click(screen.getByRole('button', { name: /delete/i }));
+  await waitFor(() =>
+    expect(screen.getByRole('heading', { name: /confirm/i })).toBeInTheDocument(),
+  );
+  userEvent.click(screen.getByRole('button', { name: /ok/i }));
+
+  await waitFor(() =>
+    expect(screen.queryByRole('heading', { name: /confirm/i })).not.toBeInTheDocument(),
+  );
+  await waitFor(() => expect(mockApi.history.delete).toHaveLength(1));
+  expect(mockApi.history.delete[0].url).toBe('/api/budgets/7');
+  expect(invalidateQueries).toHaveBeenCalledWith(['budgets', { ledger: '2' }]);
+
+  await waitFor(() => expect(history.location.pathname).toBe('/budgets'));
 });
