19655d909a3c36cd710df7d78bac3a32fda29158
Kyle Treubig <kyle@vimofthevine.com>
2021-11-28 15:28:54 -0500

    Remove unused prompt/nav hooks

--- webapp/src/common/hooks/usePromptWhenDirty.js
+++
@@ -1,7 +0,0 @@
-import { useFormikContext } from 'formik';
-import { usePrompt } from 'react-router-dom';
-
-export default (message = 'You have unsaved changes. Are you sure you wish to leave?') => {
-  const { dirty } = useFormikContext();
-  usePrompt(message, dirty);
-};
